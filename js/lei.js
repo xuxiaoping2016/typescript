e05b0a129c0df4963db08c7016424f5f4f344bfb
xiaoping.xu <xiaoping.xu@weimob.com>
2019-02-25 18:57:52 +0800

    df

--- js/lei.js
+++ js/lei.js
@@ -1,55 +1,73 @@
 "use strict";
 /**
  * 类 继承
+ * Property 'name' is protected and only accessible within class 'Animal' and its subclasses
+ * Property 'name' is private and only accessible within class 'Animal'
+ *
+ * TypeScript使用的是结构性类型系统。 当我们比较两种不同的类型时，并不在乎它们从何处而来，
+ * 如果所有成员的类型都是兼容的，我们就认为它们的类型是兼容的。
  */
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Animal = /** @class */ (function () {
-    function Animal(theName) {
-        this.name = theName;
+// class Animal {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+//     move(distanceInMeters: number = 0) {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`);
+//     }
+// }
+// class Snake extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 5) {
+//         console.log("Slithering...");
+//         super.move(distanceInMeters);
+//     }
+// }
+// let ani = new Animal("luotuo")
+// let sam = new Snake("Sammy the Python");
+// console.log(ani.name)
+// ani.move()
+// sam.move();
+// class Animal {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+// class Rhino extends Animal {
+//     constructor() { super("Rhino"); }
+// }
+// class Employee {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+// let animal = new Animal("Goat");
+// let rhino = new Rhino();
+// let employee = new Employee("Bob");
+// animal = rhino;
+// animal = employee; // 错误: Animal 与 Employee 不兼容.
+/**  存取器
+ *
+ */
+var passcode = "secret passcode";
+var Employee = /** @class */ (function () {
+    function Employee() {
     }
-    Animal.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 0; }
-        console.log(this.name + " moved " + distanceInMeters + "m.");
-    };
-    return Animal;
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                console.log("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee;
 }());
-var Snake = /** @class */ (function (_super) {
-    __extends(Snake, _super);
-    function Snake(name) {
-        return _super.call(this, name) || this;
-    }
-    Snake.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 5; }
-        console.log("Slithering...");
-        _super.prototype.move.call(this, distanceInMeters);
-    };
-    return Snake;
-}(Animal));
-var Horse = /** @class */ (function (_super) {
-    __extends(Horse, _super);
-    function Horse(name) {
-        return _super.call(this, name) || this;
-    }
-    Horse.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 45; }
-        console.log("Galloping...");
-        _super.prototype.move.call(this, distanceInMeters);
-    };
-    return Horse;
-}(Animal));
-var sam = new Snake("Sammy the Python");
-var tom = new Horse("Tommy the Palomino");
-sam.move();
-tom.move(34);
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
--- lei.ts
+++ lei.ts
@@ -1,33 +1,84 @@
 /**
  * 类 继承
+ * Property 'name' is protected and only accessible within class 'Animal' and its subclasses
+ * Property 'name' is private and only accessible within class 'Animal'
+ * 
+ * TypeScript使用的是结构性类型系统。 当我们比较两种不同的类型时，并不在乎它们从何处而来，
+ * 如果所有成员的类型都是兼容的，我们就认为它们的类型是兼容的。
  */
 
-class Animal {
-    name: string;
-    constructor(theName: string) { this.name = theName; }
-    move(distanceInMeters: number = 0) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
+// class Animal {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+//     move(distanceInMeters: number = 0) {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`);
+//     }
+// }
+
+// class Snake extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 5) {
+//         console.log("Slithering...");
+//         super.move(distanceInMeters);
+//     }
+// }
+
+// let ani = new Animal("luotuo")
+// let sam = new Snake("Sammy the Python");
+
+// console.log(ani.name)
+// ani.move()
+// sam.move();
+
+
+
+
+// class Animal {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+
+// class Rhino extends Animal {
+//     constructor() { super("Rhino"); }
+// }
+
+// class Employee {
+//     private name: string;
+//     constructor(theName: string) { this.name = theName; }
+// }
+
+// let animal = new Animal("Goat");
+// let rhino = new Rhino();
+// let employee = new Employee("Bob");
+
+// animal = rhino;
+// animal = employee; // 错误: Animal 与 Employee 不兼容.
+
+/**  存取器
+ * 
+ */
+
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
     }
 }
 
-class Snake extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 5) {
-        console.log("Slithering...");
-        super.move(distanceInMeters);
-    }
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
 }
-
-class Horse extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 45) {
-        console.log("Galloping...");
-        super.move(distanceInMeters);
-    }
-}
-
-let sam = new Snake("Sammy the Python");
-let tom: Animal = new Horse("Tommy the Palomino");
-
-sam.move();
-tom.move(34);
